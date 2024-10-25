f3e9f4d3a440dd367b8799297fafba356a8f8873
saimonproyectsv2 <simon.patino@udea.edu.co>
2024-10-25 01:23:10 -0500

    Correction integrative accumulation error

--- PID.cpp
+++ PID.cpp
@@ -14,49 +14,36 @@
 float PID::PID_iteration(float SP, float actualValue){
 
   errors[2] = SP - actualValue;
-  
   e_proportional =  errors[2];
 
   if (_SW){
-
-  e_integrative = (2*_h)*(errors[0] + 4*errors[1] + errors[2])/3;
-
+  e_integrative = (_h)*(errors[1] + errors[2])/2;
   } else {
-
   e_integrative = 0;
-
   }
+
+  e_integrative_acumulation += e_integrative;
 
   e_derivative = (3*errors[2] - 4*errors[1] + errors[0])/(2*_h);
 
-  u = _Kp*e_proportional + _Ki*e_integrative + _Kd*e_derivative; 
+  //Action control 
+  u = _Kp*e_proportional + _Ki*e_integrative_acumulation + _Kd*e_derivative; 
 
   //System of Anti-Windup
-
   if ( u > _saturation_upper || u < _saturation_lower ){
-
     _SW = false;
-
   } else {
-
     _SW = true;
-
   } 
 
   //Saturation limits
-
   if ( u > _saturation_upper) {
-
     u = _saturation_upper;
-
   } else if (u < _saturation_lower) {
-
     u = _saturation_lower;
-
   }
 
   errors[1] =  errors[2];
-
   errors[0] =  errors[1];
 
   return u ;
